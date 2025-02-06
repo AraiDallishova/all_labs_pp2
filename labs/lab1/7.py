9328d73508d02bffc7cad054f352c03d707528c2
AraiDallishova <araidallishova@gmail.com>
2025-02-06 23:02:01 +0500

    Update 7.py

--- labs/lab1/7.py
+++ labs/lab1/7.py
@@ -1,6 +1,6 @@
-x = str(7)    # x will be '3'
-y = int(8)    # y will be 3
-z = float(9)  # z will be 3.0
+x = str(7)    # x will be '7'
+y = int(8)    # y will be 8
+z = float(9)  # z will be 9.0
 print(x)
 print(y)
 print(z)
