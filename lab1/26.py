6e522411ad08051abea31d0b8f518e391d654c99
AraiDallishova <araidallishova@gmail.com>
2025-02-26 11:51:35 +0500

    добавлено файл lab_5

--- lab1/26.py
+++
@@ -1,8 +0,0 @@
-a = "Hello, World!"
-b = "Hello, World!"
-c = " Hello, World! "
-e = "Hello, World!"
-print(e.replace("H", "J"))
-print(c.strip()) 
-print(b.lower())
-print(a.upper())
--- lab1/5.py
+++
@@ -1,6 +0,0 @@
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("I add comment written in more than just one line")
