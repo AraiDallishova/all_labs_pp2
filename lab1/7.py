e2a9f4612ffb54569e1d6db3cf4855bcb8d324da
AraiDallishova <araidallishova@gmail.com>
2025-02-26 12:10:34 +0500

    Восстановлены лабораторные работы

---
+++ lab1/7.py
@@ -0,0 +1,6 @@
+x = str(7)    # x will be '7'
+y = int(8)    # y will be 8
+z = float(9)  # z will be 9.0
+print(x)
+print(y)
+print(z)
---
+++ lab2/3_listItems_exercises.py
@@ -0,0 +1,78 @@
+#1
+#What will be the result of the following syntax:
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[-1]) #cherry
+
+#2
+#Print the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1]) #banana
+
+#3
+#What will be the result of the following syntax:
+myylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
+print(myylist[1:4]) #['banana', 'cherry', 'orange']
+
+#4
+#Use a range of indexes to print the third, fourth, and fifth item in the list.
+fruiits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruiits[2:5]) #["cherry", "orange", "kiwi"]
+
+#Python Change Lists
+#1
+#What will be the result of the following syntax:
+myllist = ['apple', 'banana', 'cherry']
+myllist[0] = 'kiwi'
+print(myllist[1]) #banana
+
+#2
+#Change the value from "apple" to "kiwi", in the fruits list.
+fruitss = ["apple", "banana", "cherry"]
+fruitss[0] = "kiwi"
+
+#3
+#What will be the result of the following syntax:
+myllist = ['apple', 'banana', 'cherry']
+myllist[1:2] = ['kiwi', 'mango']
+print(myllist[2]) #mango
+
+#4
+mmyfruits = ['apple', 'banana', 'cherry']
+mmyfruits.insert(0, 'orange')
+print(mmyfruits[1]) #apple
+
+#5
+fruitsss = ["apple", "banana", "cherry"]
+fruitsss.append("orange")
+
+#6
+fruiits = ["apple", "banana", "cherry"]
+fruiits.insert(1,"orange")
+
+#7
+#Select the missing parts to add the elements of tropical to fruits:
+fruitts = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruitts.extend(tropical)
+
+#8 What is a List method for removing list items? #pop
+
+#9
+#Use the remove method to remove "banana" from the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#10
+#What will be the result of the following syntax:
+mylist = ['apple', 'banana', 'cherry']
+mylist.pop(1)
+print(mylist) #['apple','cherry']
+
+#11
+#Select the correct function to remove all items from a list:
+fruits = ['apple', 'banana', 'cherry']
+fruits.clear()
+
+
+
+
